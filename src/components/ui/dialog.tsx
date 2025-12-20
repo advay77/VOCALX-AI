8fafdaba7ce903a25fe7eb7f970bdfc4bf0f8187
SYED MOHAMMAD AQUIB <syedmohammadaquib12@gmail.com>
2025-12-21 04:02:17 +0530

    resume

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -69,7 +69,7 @@ function DialogContent({
         {showCloseButton && (
           <DialogPrimitive.Close
             data-slot="dialog-close"
-            className="absolute top-3 right-3 p-1 text-white/80 hover:text-white focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-white/30 focus:ring-offset-transparent transition duration-150 bg-transparent disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-5 dark:text-white/80 dark:hover:text-white dark:focus:ring-white/30"
+            className="absolute top-3 right-3 p-1 text-slate-900/70 hover:text-slate-900 dark:text-white/80 dark:hover:text-white focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-slate-400 dark:focus:ring-white/30 focus:ring-offset-transparent transition duration-150 bg-transparent disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-5"
           >
             <XIcon />
             <span className="sr-only">Close</span>
